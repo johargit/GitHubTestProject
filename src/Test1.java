2340aaac562812063c1379d8e3fe21a6ac8b07a2
bindhya <bindhya@PC>
2020-12-17 13:33:18 +0530

    This my First Commit

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.println("I need a food to eat");
+		} else {
+			System.out.println("No need Food");
+		}
+
+	}
+
+}
